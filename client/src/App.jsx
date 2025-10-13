304d489d02a24dfb4db0f31d42cf83b1768d3983
mahesh cheera <60442851+mynnach@users.noreply.github.com>
2025-10-13 18:25:08 -0400

    login and registration code update
    login and registration code update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,18 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
   return (
-    <div style={{ textAlign: "center", marginTop: "50px" }}>
-      <h1>Food Donation App</h1>
-      <p>Welcome to the Food Donation PWA APP</p>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </Router>
   );
 }
 
